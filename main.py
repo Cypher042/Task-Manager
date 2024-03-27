6d373c36428f8d86aaac4e73026ca4975fb4ba19
Cypher042 <ayush200501@gmail.com>
2024-03-27 19:59:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,17 @@
-import discord
-from discord.ext import commands
-from discord import Interaction
-
-client = commands.Bot(command_prefix="!", intents=discord.Intents.all())
+import nextcord
+from nextcord.ext import commands
+from nextcord import Interaction
+import os
+intents=nextcord.Intents.default()
+intents.members=True
+client = commands.Bot(command_prefix="!", intents=intents)
 
 @client.event
 async def on_ready():
-    await client.change_presence(activity=discord.activity.Game(name = "Oraganizing Tasks"))
-    print(f"{client.user.name} is logged in")
+    print("logged in")
 
-@client.command()
-async def hi(ctx):
-    await ctx.send("Hello :3!")
+@client.slash_command(guild_ids=[1217909401097080883])
+async def test(interaction : Interaction):
+    await interaction.response.send_message("hi")
 
-@client.tree.command()
-async def ping(interaction : Interaction):
-    bot_latency = round(client.latency*1000)
-    await interaction.response.send_message(f"Pong!.. {bot_latency}")
-
-client.run("MTIyMjQ1OTA3NDc2ODE0NjQ4Ng.GQ1ioF.YhEOI5IblkYXR18eYVOkL-WHuOIV3cQAeHONHA")
+client.run(TOKEN)
