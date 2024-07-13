6030b31bdb52544d01e50a0be1e8f98bb8925018
Cypher <153519644+Cypher042@users.noreply.github.com>
2024-07-13 18:46:48 +0530

    Create perms.py

---
+++ perms.py
@@ -0,0 +1,12 @@
+
+perms = {"archive_ctf":["sys-admin", "root" ],
+         "list_challenges":["sys-admin", "root","Club"],
+         "fetchchallenges":["sys-admin", "root"],
+         "done":["sys-admin", "root","Club"],
+         "status":["sys-admin", "root"],
+         "clearchalls":["sys-admin", "root" ],
+         "show_flag":["sys-admin", "root", "Club"],
+         "uploadtask":["sys-admin", "root"],
+         "showtask":["sys-admin", "root","Club"],
+         "taskdone":["sys-admin", "root"],
+         "submit_flag": ["sys-admin", "root"]}
